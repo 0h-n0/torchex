202ed99f45126168c73246ad8d6f04b7330f933c
0h-n0 <kbu94982@gmail.com>
2018-11-09 12:12:07 +0900

    feat: update ci.

--- torchex/nn/modules/embedding.py
+++ torchex/nn/modules/embedding.py
@@ -1,23 +1,21 @@
 import torch.nn as nn
 
-
 class LSTMStep(nn.Module):
-  """Layer that performs a single step LSTM update.
-  This layer performs a single step LSTM update. Note that it is *not*
-  a full LSTM recurrent network. The LSTMStep layer is useful as a
-  primitive for designing layers such as the AttnLSTMEmbedding or the
-  IterRefLSTMEmbedding below.
-  """
-
-  def __init__(self,
-               output_dim,
-               input_dim,
-               init_fn=initializations.glorot_uniform,
-               inner_init_fn=initializations.orthogonal,
-               activation_fn=activations.tanh,
-               inner_activation_fn=activations.hard_sigmoid,
-               **kwargs):
-      pass
+    """Layer that performs a single step LSTM update.
+    This layer performs a single step LSTM update. Note that it is *not*
+    a full LSTM recurrent network. The LSTMStep layer is useful as a
+    primitive for designing layers such as the AttnLSTMEmbedding or the
+    IterRefLSTMEmbedding below.
+    """
+    def __init__(self,
+                 output_dim,
+                 input_dim,
+                 init_fn=initializations.glorot_uniform,
+                 inner_init_fn=initializations.orthogonal,
+                 activation_fn=activations.tanh,
+                 inner_activation_fn=activations.hard_sigmoid,
+                 **kwargs):
+        pass
 
 
 class AttnLSTMEmbedding(nn.Module):
@@ -53,6 +51,6 @@ class IterRefLSTMEmbedding(nn.Module):
     on each other. This allows the learnable metric to be more malleable
     than that from AttnLSTMEmbeding.
     """
-
-    def __init__(self, n_test, n_support, n_feat, max_depth, **kwargs):
     
+    def __init__(self, n_test, n_support, n_feat, max_depth, **kwargs):
+        pass
