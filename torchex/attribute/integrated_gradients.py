57e878d371d85f4212c92ae8c11b4ae092c322c2
0h-n0 <kbu94982@gmail.com>
2018-11-26 00:48:18 +0900

    feat: add lazy linear.

--- torchex/attribute/integrated_gradients.py
+++ torchex/attribute/integrated_gradients.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 
+
 class IntegratedGradients(object):
     '''
 
---
+++ torchex/nn/modules/lazy/linear.py
@@ -0,0 +1,24 @@
+
+class Linear(nn.Module):
+    def __init__(self, out_features, use_bias=True):
+        super(LazyLinear, self).__init__()
+        self.out_features = out_features
+        self.use_bias = use_bias
+        
+        self.initialize = True        
+        self.weight = None
+        self.bias = None
+        
+    def forward(self, x):
+        _, in_features = x.shape
+        if self.initialize:
+            self.weight =  nn.Parameter(torch.Tensor(self.out_features, in_features))
+            stdv = 1. / math.sqrt(self.weight.size(1))
+            self.weight.data.uniform_(-stdv, stdv)
+            if self.use_bias:
+                self.bias = nn.Parameter(torch.Tensor(self.out_features))
+                self.bias.data.uniform_(-stdv, stdv)
+            self.initialize = False
+            
+        return F.linear(x, self.weight, self.bias)
+    
