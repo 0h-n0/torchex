e037cf80c990d815db6af17c132529a28b9eecf8
0h-n0 <kbu94982@gmail.com>
2018-12-05 12:18:14 +0900

    feat: add test on gpu.

--- test/nn/modules/lazy/test_lazyconv.py
+++ test/nn/modules/lazy/test_lazyconv.py
@@ -1,8 +1,10 @@
+import pytest
 import torch
 import torchex.nn as exnn
 
 
-def test_conv1d():
+
+def test_conv1d_print():
     net = exnn.Conv1d(3, 2)
     print(net)
 
@@ -23,4 +25,18 @@ def test_conv3d():
     x = torch.randn(10, 20, 28, 28, 28)
     y = net(x)
     assert list(y.shape) == [10, 3, 27, 27, 27]
-    
+
+
+@pytest.mark.skipif(not torch.cuda.is_available(), reason='No GPU')
+def test_conv1d():
+    net = exnn.Conv1d(3, 2).to('cuda')
+    x = torch.randn(10, 10, 20).to('cuda')
+    y = net(x)
+    assert list(y.shape) == [10, 3, 19]
+
+@pytest.mark.skipif(not torch.cuda.is_available(), reason='No GPU')
+def test_conv2d():
+    net = exnn.Conv2d(3, 2).to('cuda')
+    x = torch.randn(10, 20, 28, 28).to('cuda')
+    y = net(x)
+    assert list(y.shape) == [10, 3, 27, 27]
