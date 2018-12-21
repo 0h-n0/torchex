90ab3c547f21ab3a23bc4a0e7b65ffa1237a72f5
Taro Kiritani <taro.kiritani@exwzd.com>
2018-12-21 04:04:50 +0000

    fixes a typo.

--- torchex/nn/modules/cnn_blocks.py
+++ torchex/nn/modules/cnn_blocks.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 from .lazy import Conv2d
 
-class Conv2dBlock(nn.Moulde):
+class Conv2dBlock(nn.Module):
     def __init__(self, out_channels, kernel_size, stride=1,
                  padding=0, dilation=1, groups=1, bias=True,
                  dropout:float=0.0, pooling=False, batchnorm=False,
